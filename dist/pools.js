6272cf1a724c9e689277b9475f50d994bbb7c171
fred <fredlacs@gmail.com>
2021-01-26 16:32:48 -0300

    Add changes

--- dist/pools.js
+++ dist/pools.js
@@ -1,43 +1,113 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const isomorphic_fetch_1 = __importDefault(require("isomorphic-fetch"));
-const bmath = __importStar(require("./bmath"));
+'use strict';
+var __awaiter =
+    (this && this.__awaiter) ||
+    function(thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function(resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function(resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator['throw'](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            );
+        });
+    };
+var __importDefault =
+    (this && this.__importDefault) ||
+    function(mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+var __importStar =
+    (this && this.__importStar) ||
+    function(mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null)
+            for (var k in mod)
+                if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+        result['default'] = mod;
+        return result;
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
+const isomorphic_fetch_1 = __importDefault(require('isomorphic-fetch'));
+const bmath = __importStar(require('./bmath'));
 class POOLS {
     getAllPublicSwapPools(URL) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const result = yield isomorphic_fetch_1.default(URL);
+        return __awaiter(this, void 0, void 0, function*() {
+            const url =
+                'https://graph.offchainlabs.com/subgraphs/name/balancer-labs/balancer-subgraph';
+            const query = `{
+            pools(first: 1000, orderBy: liquidity, orderDirection: desc,
+                    where: {
+                    publicSwap: true,
+                    active: true,
+                    tokensCount_gt: 1
+                }) {
+                id
+                finalized
+                crp
+                tokens {
+                address
+                symbol
+                name
+                }
+                tokensList
+                totalSwapVolume
+                swaps(first: 1, orderBy: timestamp, orderDirection: desc) {
+                poolTotalSwapVolume
+                }
+            }
+        }`;
+            const result = yield isomorphic_fetch_1.default(url, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ query }),
+            });
+            // const result = await fetch(URL);
             const allPools = result.json();
             return allPools;
         });
     }
     formatPoolsBigNumber(pools) {
-        return __awaiter(this, void 0, void 0, function* () {
+        return __awaiter(this, void 0, void 0, function*() {
             let onChainPools = { pools: [] };
             for (let i = 0; i < pools.pools.length; i++) {
                 let tokens = [];
                 let p = {
                     id: pools.pools[i].id,
-                    swapFee: bmath.scale(bmath.bnum(pools.pools[i].swapFee), 18),
-                    totalWeight: bmath.scale(bmath.bnum(pools.pools[i].totalWeight), 18),
+                    swapFee: bmath.scale(
+                        bmath.bnum(pools.pools[i].swapFee),
+                        18
+                    ),
+                    totalWeight: bmath.scale(
+                        bmath.bnum(pools.pools[i].totalWeight),
+                        18
+                    ),
                     tokens: tokens,
                     tokensList: pools.pools[i].tokensList,
                 };
@@ -45,9 +115,15 @@ class POOLS {
                     let decimals = Number(token.decimals);
                     p.tokens.push({
                         address: token.address,
-                        balance: bmath.scale(bmath.bnum(token.balance), decimals),
+                        balance: bmath.scale(
+                            bmath.bnum(token.balance),
+                            decimals
+                        ),
                         decimals: decimals,
-                        denormWeight: bmath.scale(bmath.bnum(token.denormWeight), 18),
+                        denormWeight: bmath.scale(
+                            bmath.bnum(token.denormWeight),
+                            18
+                        ),
                     });
                 });
                 onChainPools.pools.push(p);
--- src/wrapper.ts
+++ src/wrapper.ts
@@ -11,6 +11,7 @@ import {
 } from './types';
 import { bnum, scale } from './bmath';
 import * as sor from './index';
+import fetch from 'node-fetch';
 
 export class SOR {
     provider: BaseProvider;
@@ -30,6 +31,7 @@ export class SOR {
     MULTIADDR: { [chainId: number]: string } = {
         1: '0x514053acec7177e277b947b1ebb5c08ab4c4580e',
         42: '0x71c7f1086aFca7Aa1B0D4d73cfa77979d10D3210',
+        79377087078960: '0x65b71D9aEBfb61be7336B621f6322AB677ed3eb7',
     };
 
     constructor(
